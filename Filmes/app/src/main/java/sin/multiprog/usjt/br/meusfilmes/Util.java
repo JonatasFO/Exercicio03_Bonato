4bc9902845b49e840a7a7faa57c794c1ab21644d
JonatasFO <jonatas_098@hotmail.com>
2018-09-30 23:03:34 -0300

    Lab03

---
+++ Filmes/app/src/main/java/sin/multiprog/usjt/br/meusfilmes/Util.java
@@ -0,0 +1,30 @@
+package sin.multiprog.usjt.br.meusfilmes;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import java.lang.reflect.Field;
+
+
+/**
+*
+*Nome: Jonatas Ferreira de Oliveira RA: 818231518
+*
+*/
+
+public class Util {
+
+    public static Drawable getImagem(Context context, String nome){
+        Class<?> c = R.drawable.class;
+        try {
+            Field f = c.getDeclaredField(nome);
+            int id = f.getInt(f);
+            return context.getResources().getDrawable(id, null);
+        } catch (NoSuchFieldException e ) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return context.getDrawable(R.drawable.happy);
+    }
+}
